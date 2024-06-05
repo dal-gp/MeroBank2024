ea60a7a7431ee4b16d246e85f48024016f89a65f
Dal S Garbuja Pun <n11176491@qut.edu.au>
2024-06-05 14:39:35 +1000

    create client transaction view

---
+++ src/main/java/com/example/merobank/TransactionsController.java
@@ -0,0 +1,9 @@
+package com.example.merobank;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class TransactionsController {
+    @FXML
+    public ListView transactionsListView;
+}
