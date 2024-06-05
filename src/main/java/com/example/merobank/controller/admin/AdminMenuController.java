88adc1929ebfe29b5f1f2be1ac976a5d2b7f654a
Dal S Garbuja Pun <n11176491@qut.edu.au>
2024-06-05 22:07:21 +1000

    create admin menu view

---
+++ src/main/java/com/example/merobank/controller/admin/AdminMenuController.java
@@ -0,0 +1,20 @@
+package com.example.merobank.controller.admin;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AdminMenuController {
+
+    @FXML
+    private Button clientsButton;
+
+    @FXML
+    private Button createAccountButton;
+
+    @FXML
+    private Button depositButton;
+
+    @FXML
+    private Button logoutButton;
+
+}
