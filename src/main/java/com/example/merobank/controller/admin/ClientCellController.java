f94c1acbb82d152eb84d117c5cafb06c1e22e97b
Dal S Garbuja Pun <n11176491@qut.edu.au>
2024-06-06 15:24:25 +1000

    create client cell view

---
+++ src/main/java/com/example/merobank/controller/admin/ClientCellController.java
@@ -0,0 +1,30 @@
+package com.example.merobank.controller.admin;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ClientCellController {
+
+    @FXML
+    private Label checkingAccountLabel;
+
+    @FXML
+    private Label dateLabel;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Label firstNameLabel;
+
+    @FXML
+    private Label lastNameLabel;
+
+    @FXML
+    private Label payeeAddressLabel;
+
+    @FXML
+    private Label savingsAccountLabel;
+
+}
