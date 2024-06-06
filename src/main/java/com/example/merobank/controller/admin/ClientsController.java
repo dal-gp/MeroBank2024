e80ec74e0df08ec12492db8a4fe37fa11760ebe8
Dal S Garbuja Pun <n11176491@qut.edu.au>
2024-06-06 11:11:24 +1000

    create admin clients view

---
+++ src/main/java/com/example/merobank/controller/admin/ClientsController.java
@@ -0,0 +1,11 @@
+package com.example.merobank.controller.admin;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class ClientsController {
+
+    @FXML
+    private ListView<?> clientsListView;
+
+}
