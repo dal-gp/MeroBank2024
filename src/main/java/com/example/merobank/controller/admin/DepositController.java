ed91629b5ec13ade10f4f25c2563457455fb42c1
Dal S Garbuja Pun <n11176491@qut.edu.au>
2024-06-06 15:20:45 +1000

    create deposit view

---
+++ src/main/java/com/example/merobank/controller/admin/DepositController.java
@@ -0,0 +1,25 @@
+package com.example.merobank.controller.admin;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class DepositController {
+
+    @FXML
+    private TextField amountTextField;
+
+    @FXML
+    private Button despoitButton;
+
+    @FXML
+    private TextField payeeAddressTextField;
+
+    @FXML
+    private ListView<?> resultsListView;
+
+    @FXML
+    private Button searchButton;
+
+}
